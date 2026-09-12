2504f0d5c540e118a450c0914ee68e8c08502dc2
md-shahriar-kabir <shahriar.bizwork@gmail.com>
2026-09-12 22:58:57 +0600

    8th problem solved

--- index.js
+++ index.js
@@ -216,3 +216,56 @@ var removeNthFromEnd = function(head, n) {
 
     return dummy.next;
 };
+
+// ------------------------------------------------------
+// ------------------------------------------------------
+
+
+// 08. Find First and Last Position of Element in Sorted Array
+// Write a function that finds the starting and ending position of a given target value in a sorted array. Return [-1, -1] if the target does not exist.
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
+
+var searchRange = function(nums, target) {
+    let first = -1;
+    let last = -1;
+
+    // Find first position
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            first = mid;
+            right = mid - 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    // Find last position
+    left = 0;
+    right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            last = mid;
+            left = mid + 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return [first, last];
+};
