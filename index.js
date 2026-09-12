d314dcd70cbc533fe749412bb0a0285901664f8d
md-shahriar-kabir <shahriar.bizwork@gmail.com>
2026-09-12 22:52:33 +0600

    1st Problem Solved

---
+++ index.js
@@ -0,0 +1,34 @@
+
+// 01. Isomorphic Strings
+// /**
+//  * @param {string} s
+//  * @param {string} t
+//  * @return {boolean}
+//  */
+var isIsomorphic = function(s, t) {
+    if (s.length !== t.length) return false;
+
+    const mapST = {};
+    const mapTS = {};
+
+    for (let i = 0; i < s.length; i++) {
+        const charS = s[i];
+        const charT = t[i];
+
+        // s -> t mapping check
+        if (mapST[charS] && mapST[charS] !== charT) {
+            return false;
+        }
+
+        // t -> s mapping check
+        if (mapTS[charT] && mapTS[charT] !== charS) {
+            return false;
+        }
+
+        mapST[charS] = charT;
+        mapTS[charT] = charS;
+    }
+
+    return true;
+};
+
